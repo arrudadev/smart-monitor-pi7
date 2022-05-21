fef074e9720b9e8c93a23f73ec1b17ac621648d5
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2022-05-21 16:52:29 -0300

    chore: setup styled-components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,56 @@
+import { Fragment } from 'react';
+
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    context: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = context.renderPage;
+
+    try {
+      context.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(context);
+
+      return {
+        ...initialProps,
+        styles: [
+          <Fragment key="1">
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </Fragment>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
