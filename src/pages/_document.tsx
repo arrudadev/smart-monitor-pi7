356fbf1f0eab175e695b81f7e8322e302b79fa62
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2022-05-21 21:23:53 -0300

    chore: add font Roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,6 +45,15 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <meta charSet="utf-8" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
