354f64e594eb4d59cbe979d812cdf65418a3c0b1
Alexandre Monteiro Arruda Junior <alexandre.monteiro.bec@gmail.com>
2022-05-28 15:24:39 +0000

    chore: add ChartTableHeader in dashboard

--- src/pages/dashboard/_styles.dashboard.ts
+++ src/pages/dashboard/_styles.dashboard.ts
@@ -21,3 +21,85 @@ export const StatsGrid = styled.div`
 
   margin-top: 1rem;
 `;
+
+export const ChartTableHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 2rem;
+`;
+
+export const ChartTableToogleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ChartTableToogleTitle = styled.span`
+  font-size: 18px;
+  font-weight: 500;
+
+  margin-bottom: 1rem;
+`;
+
+export const ChartTableToogle = styled.div`
+  display: flex;
+  align-items: center;
+
+  border: 1px solid var(--text-secundary);
+  border-radius: 8px;
+
+  > :first-child {
+    border-right: 1px solid var(--text-secundary);
+  }
+`;
+
+export const ChartTableItem = styled.div`
+  padding: 1rem;
+
+  cursor: pointer;
+
+  color: var(--text-secundary);
+
+  &.active > svg {
+    color: var(--text);
+  }
+`;
+
+export const FilterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FilterTitle = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+
+  margin-bottom: 1rem;
+`;
+
+export const Filter = styled.div`
+  display: flex;
+  align-items: center;
+
+  border: 1px solid var(--text-secundary);
+  border-radius: 8px;
+
+  div + div {
+    border-left: 1px solid var(--text-secundary);
+  }
+`;
+
+export const FilterItem = styled.div`
+  font-size: 18px;
+
+  padding: 1rem;
+
+  cursor: pointer;
+
+  color: var(--text-secundary);
+
+  &.active {
+    color: var(--text);
+  }
+`;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,25 @@
+import { AiOutlineAreaChart } from 'react-icons/ai';
+import { FaTable } from 'react-icons/fa';
 import { RiHeartPulseFill } from 'react-icons/ri';
 
 import type { NextPage } from 'next';
 
 import { Layout } from '../../components/Layout';
 import { StatCard } from '../../components/StatCard';
-import { Container, StatsGrid, Title } from './_styles.dashboard';
+import {
+  ChartTableHeader,
+  ChartTableItem,
+  ChartTableToogle,
+  ChartTableToogleTitle,
+  ChartTableToogleWrapper,
+  Container,
+  Filter,
+  FilterItem,
+  FilterTitle,
+  FilterWrapper,
+  StatsGrid,
+  Title,
+} from './_styles.dashboard';
 
 const Dashboard: NextPage = () => (
   <Layout>
@@ -42,6 +57,36 @@ const Dashboard: NextPage = () => (
           icon={<RiHeartPulseFill color="red" size={48} />}
         />
       </StatsGrid>
+
+      <ChartTableHeader>
+        <ChartTableToogleWrapper>
+          <ChartTableToogleTitle>Visualização</ChartTableToogleTitle>
+
+          <ChartTableToogle>
+            <ChartTableItem className="active">
+              <AiOutlineAreaChart size={24} />
+            </ChartTableItem>
+
+            <ChartTableItem>
+              <FaTable size={24} />
+            </ChartTableItem>
+          </ChartTableToogle>
+        </ChartTableToogleWrapper>
+
+        <FilterWrapper>
+          <FilterTitle>Filtros</FilterTitle>
+
+          <Filter>
+            <FilterItem className="active">30</FilterItem>
+
+            <FilterItem>15</FilterItem>
+
+            <FilterItem>7</FilterItem>
+
+            <FilterItem>3</FilterItem>
+          </Filter>
+        </FilterWrapper>
+      </ChartTableHeader>
     </Container>
   </Layout>
 );
