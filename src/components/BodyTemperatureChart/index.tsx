696e69fda39777365bc980dd4356b53546dd9e23
Alexandre Monteiro Arruda Junior <alexandre.monteiro.bec@gmail.com>
2022-05-28 16:57:27 +0000

    chore: add BodyTemperatureChart component

---
+++ src/components/BodyTemperatureChart/index.tsx
@@ -0,0 +1,27 @@
+import { AreaChart } from '../AreaChart';
+import { Container, Title } from './styles';
+
+export const BodyTemperatureChart = () => {
+  const categories = [
+    '2021-11-10T10:00:00.000z',
+    '2021-11-11T11:00:00.000z',
+    '2021-11-12T12:00:00.000z',
+    '2021-11-13T13:00:00.000z',
+    '2021-11-14T14:00:00.000z',
+  ];
+
+  const series = [
+    {
+      name: 'teste',
+      data: [1, 2, 3, 4, 5],
+    },
+  ];
+
+  return (
+    <Container>
+      <Title>Média de temperatura corporal por dia</Title>
+
+      <AreaChart categories={categories} series={series} />
+    </Container>
+  );
+};
---
+++ src/components/BodyTemperatureChart/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+
+  margin-top: 1rem;
+
+  padding: 2rem;
+
+  background-color: var(--background-light);
+`;
+
+export const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 600;
+`;
