17bc8c6b7e1fbdd44ecba7cca2c63965bb0e1183
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2022-06-02 21:03:24 -0300

    chore: fetch stats from api

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AiOutlineAreaChart } from 'react-icons/ai';
 import { FaTable, FaTemperatureHigh, FaTemperatureLow } from 'react-icons/fa';
 import { MdOutlineBloodtype } from 'react-icons/md';
@@ -6,6 +6,8 @@ import { RiHeartPulseFill } from 'react-icons/ri';
 import { WiHumidity } from 'react-icons/wi';
 
 import type { NextPage } from 'next';
+
+import { Stats } from '@prisma/client';
 
 import { BodyTemperatureChart } from '../../components/BodyTemperatureChart';
 import { HeartBeatsChart } from '../../components/HeartBeatsChart';
@@ -31,8 +33,23 @@ import {
   TitleWrapper,
 } from './_styles.dashboard';
 
+type ResponseData = {
+  data: Stats[];
+};
+
 const Dashboard: NextPage = () => {
+  const [stats, setStats] = useState<Stats[]>([]);
   // const [visualization, setVisualization] = useState('chart');
+
+  async function fetchStats() {
+    fetch('/api/stats')
+      .then(response => response.json())
+      .then((data: ResponseData) => setStats(data.data));
+  }
+
+  useEffect(() => {
+    fetchStats();
+  }, []);
 
   return (
     <Layout>
@@ -40,13 +57,15 @@ const Dashboard: NextPage = () => {
         <TitleWrapper>
           <Title>Dashboard</Title>
 
-          <ReloadIcon />
+          <ReloadIcon onClick={fetchStats} />
         </TitleWrapper>
 
         <StatsGrid>
           <StatCard
             title="Batimentos Cardiacos"
-            value="89"
+            value={`${
+              stats.length > 0 ? stats[stats.length - 1].heart_beats : '0'
+            } bpm`}
             icon={<RiHeartPulseFill color="red" size={48} />}
             description="Em repouso, a frequência cardíaca normal apresenta uma variação entre 60 e 100 batimentos por minuto (bpm). A aceleração dos batimentos (acima de 100 bpm) indica que a pessoa está com taquicardia. Já uma frequência cardíaca baixa, inferior a 60 bpm, é considerada uma condição de bradicardia."
             referenceLink="https://conteudo.omronbrasil.com/batimentos-cardiacos-normais-por-idade/#:~:text=Em%20repouso%2C%20a%20frequ%C3%AAncia%20card%C3%ADaca,considerada%20uma%20condi%C3%A7%C3%A3o%20de%20bradicardia."
@@ -54,7 +73,9 @@ const Dashboard: NextPage = () => {
 
           <StatCard
             title="Spo2"
-            value="89"
+            value={`${
+              stats.length > 0 ? stats[stats.length - 1].heart_beats : '0'
+            } %`}
             icon={<MdOutlineBloodtype color="brown" size={48} />}
             description="A saturação do oxigênio é um parâmetro vital para definir o índice de oxigênio do sangue e a entrega do oxigênio. Para adultos, a escala normal do SpO2 é 95 – 100%. Um valor mais baixo de 90% é considerado a baixa saturação do oxigênio, que exige o suplemento externo do oxigênio."
             referenceLink="https://prolife.com.br/o-que-e-saturacao-de-oxigenio-e-qual-o-valor-minimo-do-spo2/#:~:text=A%20satura%C3%A7%C3%A3o%20do%20oxig%C3%AAnio%20%C3%A9,o%20suplemento%20externo%20do%20oxig%C3%AAnio."
@@ -62,7 +83,9 @@ const Dashboard: NextPage = () => {
 
           <StatCard
             title="Temperatura Corporal"
-            value="89"
+            value={`${
+              stats.length > 0 ? stats[stats.length - 1].heart_beats : '0'
+            } º`}
             icon={<FaTemperatureHigh color="#963832" size={48} />}
             description="A temperatura corpórea considerada ideal varia entre 36º C e 36,7º C. Geralmente, ela é mais baixa pela manhã e mais alta no fim da tarde ou à noite. Alterações de até um grau podem ser absolutamente aceitáveis em condições normais. Nas mulheres, por exemplo, após a ovulação, durante o ciclo menstrual e no primeiro trimestre da gravidez, ocorre uma elevação natural da temperatura."
             referenceLink="https://drauziovarella.uol.com.br/doencas-e-sintomas/febre/#:~:text=A%20temperatura%20corp%C3%B3rea%20considerada%20ideal,absolutamente%20aceit%C3%A1veis%20em%20condi%C3%A7%C3%B5es%20normais."
@@ -70,7 +93,9 @@ const Dashboard: NextPage = () => {
 
           <StatCard
             title="Temperatura no ambiente"
-            value="89"
+            value={`${
+              stats.length > 0 ? stats[stats.length - 1].heart_beats : '0'
+            } º`}
             icon={<FaTemperatureLow color="green" size={48} />}
             description="Segundo a NR17, do Ministério do Trabalho, a temperatura do ambiente de trabalho onde são executadas atividades intelectuais como nos laboratórios, escritórios, sala de desenvolvimento e projetos, deve ficar entre 20 e 23 graus centígrados"
             referenceLink="https://blog.frigelar.com.br/qual-temperatura-ideal-ambiente/"
@@ -78,7 +103,9 @@ const Dashboard: NextPage = () => {
 
           <StatCard
             title="Umidade no ambiente"
-            value="89"
+            value={`${
+              stats.length > 0 ? stats[stats.length - 1].heart_beats : '0'
+            } %`}
             icon={<WiHumidity color="blue" size={72} />}
             description="Segundo NR17, do Ministério do Trabalho define, então, que a temperatura no ambiente de trabalho deve ser entre 20 e 23 graus celsius, enquanto a umidade relativa do ar não pode ser menor do que 40%."
             referenceLink="https://blog.climabrisa.com.br/por-que-controlar-a-umidade-do-ar-no-ambiente-de-trabalho/"
@@ -139,7 +166,7 @@ const Dashboard: NextPage = () => {
           {visualization === 'table' && <Table />}
         </ChartTableWrapper> */}
 
-        <Table />
+        <Table rows={stats} />
       </Container>
     </Layout>
   );
